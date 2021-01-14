48847f55f444cda710ff0afd4126bd5fc2a46644
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-01-13 19:25:11 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+prime?(number)
+    if number <= 1
+        return false
+    elsif  
+        factors = (2..(number - 1)).to_a
+        factors.any? {|n| number % n == 0} 
+            return false  
+    else 
+        return true
+    end
+end
